1f6fc615eccc23fad40449815ac3d2c2a2281342
RemusTeo <remustck@gmail.com>
2023-03-11 03:41:05 +0800

    Implement relevant getters

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.5.0;
 
+import "./DateTime.sol";
+
 contract Event {
 
     struct eventObj {
@@ -17,7 +19,7 @@ contract Event {
     function createEvent(
         string memory title,
         string memory venue,
-        uint256 dateAndTime,
+        uint256 year, uint256 month, uint256 day, uint256 hour, uint256 minute, uint256 second,
         uint256 capacity,
         uint256 ticketsLeft,
         address seller
@@ -28,7 +30,7 @@ contract Event {
         eventObj memory newEvent = eventObj(
             title,
             venue,
-            dateAndTime,
+            DateTime.timestampFromDateTime(year, month, day, hour, minute, second),
             capacity,
             ticketsLeft,
             seller
@@ -46,6 +48,30 @@ contract Event {
     modifier validEventId(uint256 eventId) {
         require(eventId < numEvents);
         _;
+    }
+
+    function getEventTitle(uint256 eventId) public view validEventId(eventId) returns (string memory) {
+        return events[eventId].title;
+    }
+
+    function getEventVenue(uint256 eventId) public view validEventId(eventId) returns (string memory) {
+        return events[eventId].venue;
+    }
+
+    function getEventDateAndTime(uint256 eventId) public view validEventId(eventId) returns (uint256, uint256, uint256, uint256, uint256, uint256) {
+        return DateTime.timestampToDateTime(events[eventId].dateAndTime);
+    }
+
+    function getEventCapacity(uint256 eventId) public view validEventId(eventId) returns (uint256) {
+        return events[eventId].capacity;
+    }
+
+    function getEventTicketsLeft(uint256 eventId) public view validEventId(eventId) returns (uint256) {
+        return events[eventId].ticketsLeft;
+    }
+    
+    function getEventSeller(uint256 eventId) public view validEventId(eventId) returns (address) {
+        return events[eventId].seller;
     }
 }
 
