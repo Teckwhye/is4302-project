ffbcc40990c77f790878e824eb6cdb915d73417e
a-tph <aeronteoph@gmail.com>
2023-03-15 22:37:03 +0800

    Modify address to msg.sender

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -14,21 +14,21 @@ contract Account {
 
     mapping (address => account) accounts;
 
-    modifier validAccount(address addr) {
-        require(accounts[addr].registered == true); //Check if registered 
+    modifier validAccount() {
+        require(accounts[msg.sender].registered == true); //Check if registered 
         _;
     }
 
-    modifier invalidAccount(address addr) {
-        require(accounts[addr].registered == false); //Check if not registered
+    modifier invalidAccount() {
+        require(accounts[msg.sender].registered == false); //Check if not registered
         _;
     }
 
     /* Ensures an account is registered on the platform */
-    function createAccount(address addr) public invalidAccount(addr) {
-        accounts[addr].state = status.verified;
-        accounts[addr].registered = true; // updatees the field to make an account valid
-    }  
+    function createAccount() public invalidAccount() {
+        accounts[msg.sender].state = status.verified;
+        accounts[msg.sender].registered = true; // updatees the field to make an account valid
+    }
 
 
 }
