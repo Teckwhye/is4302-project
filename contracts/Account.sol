af33705aa82b9449c9442bd1ca915f2b17bdd706
a-tph <aeronteoph@gmail.com>
2023-03-17 22:40:34 +0800

    Remove unnecessary functions and variables

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -9,33 +9,18 @@ contract Account {
 
     struct account {
         status state; // whether an account can sell tickets
-        bool registered;
     }
 
     mapping (address => account) accounts;
 
-    modifier validAccount(address addr) {
-        require(accounts[addr].registered == true); //Check if registered 
-        _;
-    }
-
-    modifier invalidAccount(address addr) {
-        require(accounts[addr].registered == false); //Check if not registered
-        _;
-    }
-
     /* Ensures an account is registered on the platform */
-    function createAccount(address addr) public invalidAccount(addr) {
+    function verifyAccount(address addr) public {
         accounts[addr].state = status.verified;
-        accounts[addr].registered = true; // updatees the field to make an account valid
     }
 
+    /* View state of an account */
     function viewAccountState(address addr) public view returns (status state) {
         return accounts[addr].state;
     }
-
-    function viewAccountRegistered(address addr) public view returns(bool registered) {
-        return accounts[addr].registered;
-    } 
-
+    
 }
