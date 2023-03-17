b4ef3bfc7068a9342b116e5510643cd2c745f8ca
Teck Hwee <tan.brian98@gmail.com>
2023-03-17 03:15:53 -0700

    Fix compilation errors, now able to run truffle test

--- contracts/Platform.sol
+++ contracts/Platform.sol
@@ -4,29 +4,29 @@ import "./Buyer.sol";
 import "./Seller.sol"; 
 import "./Event.sol";
 import "./Ticket.sol";
-import "./ConcertToken.sol";
+import "./EventToken.sol";
 
 contract Platform {
     Buyer buyerContract;
     Seller sellerContract;
-    ConcertToken concertTokenContract;
+    EventToken eventTokenContract;
     Event eventContract;
 
     // Platform can only exist if other contracts are created first
-    constructor(Buyer buyerAddr, Seller sellerAddr, ConcerToken concertTokenAddr, Event eventAddr) public {
+    constructor(Buyer buyerAddr, Seller sellerAddr, EventToken eventTokenAddr, Event eventAddr) public {
         buyerContract = buyerAddr;
         sellerContract = sellerAddr;
-        concertTokenContract = concertTokenAddr;
+        eventTokenContract = eventTokenAddr;
         eventContract = eventAddr;
     }
 
     /* Viewing the number of tickets left for an event */
-    function viewTicketsLeft(eventId) public view returns (uint256) {
+    function viewTicketsLeft(uint256 eventId) public view returns (uint256) {
         return eventContract.getEventTicketsLeft(eventId);
     }
 
     /* Buyers buying tickets for an event */
-    function buyTickets(eventId, quantity) public payable {
+    function buyTickets(uint256 eventId, uint8 quantity) public payable {
         // buyerContract.buyTickets(eventId, quantity, concertTokenContract);
     }
 
