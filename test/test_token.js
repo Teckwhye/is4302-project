70801e5eb4638be9998b4bbe376a41ea2412a898
Teck Hwee <tan.brian98@gmail.com>
2023-03-17 02:58:55 -0700

    Update changes to test_token.js

--- test/test_token.js
+++ test/test_token.js
@@ -3,30 +3,30 @@ const truffleAssert = require("truffle-assertions"); // npm truffle-assertions
 const BigNumber = require("bignumber.js"); // npm install bignumber.js
 var assert = require("assert");
 
-var ConcertToken = artifacts.require("../contracts/ConcertToken.sol");
+var EventToken = artifacts.require("../contracts/EventToken.sol");
 
-contract("ConcertToken", function (accounts) {
+contract("EventToken", function (accounts) {
     before(async () => {
-        concertTokenInstance = await ConcertToken.deployed();
+        eventTokenInstance = await EventToken.deployed();
     });
     
-    console.log("Testing ConcertToken contract");
+    console.log("Testing EventToken contract");
 
-    it("Get Concert Token", async () => {
-        let totalSupplyBefore = await concertTokenInstance.getCurrentSupply({from: accounts[1]});
-        await concertTokenInstance.getConcertToken({from: accounts[1], value: 5000000});
-        let totalSupplyAfter = await concertTokenInstance.getCurrentSupply({from: accounts[1]});
+    it("Get Event Token", async () => {
+        let totalSupplyBefore = await eventTokenInstance.getCurrentSupply({from: accounts[1]});
+        await eventTokenInstance.getEventToken({from: accounts[1], value: 5000000});
+        let totalSupplyAfter = await eventTokenInstance.getCurrentSupply({from: accounts[1]});
         let totalSupplyAdded = totalSupplyAfter - totalSupplyBefore;
-        assert.equal(await concertTokenInstance.checkConcertToken({from: accounts[1]}), 100);
+        assert.equal(await eventTokenInstance.checkEventToken({from: accounts[1]}), 100);
         assert.equal(totalSupplyAdded, 100);
     });
 
     it("Refund Conert Token", async () => {
-        let totalSupplyBefore = await concertTokenInstance.getCurrentSupply({from: accounts[1]});
-        await concertTokenInstance.refundConcertToken(100,{from: accounts[1]});
-        let totalSupplyAfter = await concertTokenInstance.getCurrentSupply({from: accounts[1]});
+        let totalSupplyBefore = await eventTokenInstance.getCurrentSupply({from: accounts[1]});
+        await eventTokenInstance.refundEventToken(100,{from: accounts[1]});
+        let totalSupplyAfter = await eventTokenInstance.getCurrentSupply({from: accounts[1]});
         let totalSupplyRemoved = totalSupplyBefore - totalSupplyAfter;
-        assert.equal(await concertTokenInstance.checkConcertToken({from: accounts[1]}), 0);
+        assert.equal(await eventTokenInstance.checkEventToken({from: accounts[1]}), 0);
         assert.equal(totalSupplyRemoved, 100);
     });
 })
