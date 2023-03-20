352ebcbd11af82ffb889d388c3ae83fa6f54cd9f
RemusTeo <remustck@gmail.com>
2023-03-20 21:31:04 +0800

    Basic logic of bidding

--- contracts/Platform.sol
+++ contracts/Platform.sol
@@ -20,6 +20,37 @@ contract Platform {
         eventContract = eventAddr;
     }
 
+    mapping(uint256 => mapping(uint256 => address[])) public eventBiddings; // eventId => (tokenBid => addressArray)
+    mapping(uint256 => uint256) public eventTopBid; // eventId => topBid (need to constantly update eventTopBid)
+    
+    // Commence bidding for event
+    function commenceBidding(uint256 eventId) public {
+        //require(eventBiddings[eventId] == 0);
+    }
+
+    // Bid
+    function addBid(uint256 eventId, uint256 tokenBid) public {
+        //require 
+        eventBiddings[eventId][tokenBid].push(msg.sender);
+    }
+
+    // Close bidding and 
+    function closeBidding(uint256 eventId) public {
+        // compare top tickets
+
+        uint256 bidAmount = eventTopBid[eventId];
+        uint256 ticketsLeft = eventContract.getEventTicketsLeft(eventId);
+
+        while (ticketsLeft != 0) {
+            address[] memory bidderList = eventBiddings[eventId][bidAmount];
+            for (uint256 i = 0; i < bidderList.length; i++) {
+                // transfer ticket to address @ bidderList[i]
+                ticketsLeft--;
+            }
+            bidAmount--;
+        }
+    }
+
     /* Viewing the number of tickets left for an event */
     function viewTicketsLeft(uint256 eventId) public view returns (uint256) {
         return eventContract.getEventTicketsLeft(eventId);
