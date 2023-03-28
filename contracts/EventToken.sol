ed58c2681389c117322bf4e57d81b621785515fd
Teck Hwee <tan.brian98@gmail.com>
2023-03-28 05:29:22 -0700

    Added more functions to event token market and algo

--- contracts/EventToken.sol
+++ contracts/EventToken.sol
@@ -7,6 +7,8 @@ contract EventToken {
     //uint256 supplyLimit;
     uint256 currentSupply;
     uint256 basePriceOfToken;
+    uint256 numberOfUsers;
+    mapping (address => bool) UsersWithTokens;
     mapping(address => bool) allowedRecipient; // mapping of addresses that is able to recieve transfer of tokens from users
     mapping(address => bool) authorisedAddress; // mapping od addresses authorised to use certain functions
     address owner;
@@ -24,6 +26,7 @@ contract EventToken {
         basePriceOfToken = 50000;
         allowedRecipient[owner] = true;
         authorisedAddress[owner] = true;
+        numberOfUsers = 0;
     }
 
     modifier onlyOwner() {
@@ -74,6 +77,10 @@ contract EventToken {
         uint256 amtOfToken = amtOfWei / basePriceOfToken;
         erc20Contract.mint(_to, amtOfToken);
         currentSupply = currentSupply + amtOfToken;
+        if (UsersWithTokens[_to] == false) { // New user
+            numberOfUsers = numberOfUsers + 1;
+            UsersWithTokens[_to] = true;
+        }
         emit MintToken(_to, amtOfToken);
     }
 
@@ -113,6 +120,16 @@ contract EventToken {
     // Check current amount of supply of tokens in the market
     function getCurrentSupply() public view returns (uint256) {
         return currentSupply;
+    }
+
+    // Check current amount of users in the market
+    function getNumberOfUsers() public view returns (uint256) {
+        return numberOfUsers;
+    }
+
+    // get base price of tokens
+    function getBasePriceOfToken() public view returns (uint256) {
+        return basePriceOfToken;
     }
 
 }
--- contracts/EventTokenMarketAlgorithm.sol
+++ contracts/EventTokenMarketAlgorithm.sol
@@ -1,4 +1,7 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+import "./EventToken.sol";
 
 contract EventTokenMarketAlgorithm {
 
@@ -16,8 +19,10 @@ contract EventTokenMarketAlgorithm {
     }
 
     address eventTokenMarketAddress;
-    uint256 sellOrderFirstNum;                                       // Keep track of first order
-    uint256 sellOrderLastNum;                                        // Keep track of last order
+    EventToken eventTokenContract; 
+    uint256 sellOrderFirstNum;                                       // Keep track of first sell order
+    uint256 sellOrderLastNum;                                        // Keep track of last sell order
+    uint256 currentSellQuantity;
     mapping(uint256 => sellOrder) sellOrderList;
     
 
@@ -26,12 +31,15 @@ contract EventTokenMarketAlgorithm {
     /**
      * creates EventTokenMarketAlgorithm to maintain order of market
      *
-     * param _eventTokenMarketAddress    address of event token market for sale of tokens     
+     * param _eventTokenMarketAddress    address of event token market for sale of tokens
+     * param eventTokenAddress           address of event token to check current supply to decide price of tokens     
      */
-    constructor(address _eventTokenMarketAddress) public {
+    constructor(address _eventTokenMarketAddress, EventToken eventTokenAddress) public {
         sellOrderFirstNum = 0;
         sellOrderLastNum = 0;
+        currentSellQuantity = 0;
         eventTokenMarketAddress = _eventTokenMarketAddress;
+        eventTokenContract = eventTokenAddress;
     }
 
     // modifier to ensure only market algorithm can do this
@@ -55,6 +63,8 @@ contract EventTokenMarketAlgorithm {
         sellOrder memory newSellOrder = sellOrder(_seller, _quantity);
         // List order
         sellOrderList[sellOrderId] = newSellOrder;
+        // Add total sell quantity
+        currentSellQuantity = currentSellQuantity + _quantity;
         // Increment number for next order id
         sellOrderLastNum = sellOrderLastNum + 1;
         // Return order id
@@ -77,6 +87,59 @@ contract EventTokenMarketAlgorithm {
     }
 
     /**
+     * All checks to be done on EventTokenMarket
+     * buy tokens
+     *
+     * param sellOrderId id of sell order to remove
+     */
+    function buyTokens(uint256[2] memory order) onlyEventTokenMarketAddress public returns (uint256[] memory, address[] memory, uint256){
+
+        uint256 quantityToBuy = order[0];
+        uint256[] memory quantities;
+        address[] memory sellers;
+        uint256 startnum = 0;
+        while (sellOrderFirstNum <= sellOrderLastNum && quantityToBuy > 0) { // There is still tokens to sell
+            uint256 quantityAvailable = sellOrderList[sellOrderFirstNum].quantity;
+            if (quantityAvailable > quantityToBuy) { // Seller can take entire order
+                quantities[startnum] = quantityToBuy;
+                sellers[startnum] = sellOrderList[sellOrderFirstNum].seller;
+                sellOrderList[sellOrderFirstNum].quantity = sellOrderList[sellOrderFirstNum].quantity - quantityToBuy;
+                break;
+            } 
+            else {
+                quantityToBuy = quantityToBuy - quantityAvailable;
+                quantities[startnum] = quantityAvailable;
+                sellers[startnum] = sellOrderList[sellOrderFirstNum].seller;
+                sellOrderList[sellOrderFirstNum].quantity = 0;
+                sellOrderFirstNum = sellOrderFirstNum + 1;
+                startnum = startnum + 1;
+            }
+        }
+        startnum = startnum + 1;
+        return (quantities, sellers, startnum);
+    }
+
+    /**
+     * decide price of tokens (Can brainstorm the algorithm, just a sample one for now)
+     *
+     * param quantity of tokens to sell
+     * returns price and token quantity
+     */
+    function getPriceOfBuyOrder(uint256 _quantity) public view returns (uint256[2] memory) {
+        uint256 basePriceOfToken = eventTokenContract.getBasePriceOfToken();
+        uint256 buyQuantity = _quantity;
+        
+        // How many percentage of token is buyer buying from the sale market? 
+        if (_quantity >= currentSellQuantity) {
+            buyQuantity = currentSellQuantity;
+        }
+        uint256 percentageOfSaleMarket = ((buyQuantity * 100 - 1) / currentSellQuantity ); // Every percentage of market will increase price of token by 10000
+        uint256 priceOfEachToken = basePriceOfToken + (10000 * percentageOfSaleMarket);
+        uint256[2] memory order = [buyQuantity, priceOfEachToken];
+        return order;
+    }
+
+    /**
      * check if sell order belongs to address
      *
      * param _address address to check
@@ -84,6 +147,14 @@ contract EventTokenMarketAlgorithm {
      */
     function isSellOrderSeller(address _address, uint256 sellOrderId) public view returns (bool) {
         return sellOrderList[sellOrderId].seller == _address;
+    }
+
+    /**
+     * check current sell quantity in market
+     *
+     */
+    function getCurrentSellQuantity() public view returns (uint256) {
+        return currentSellQuantity;
     }
     
 }
