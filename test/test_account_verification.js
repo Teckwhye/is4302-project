fb5f36adbe2ecd9881daacc4994e9790a8957ccb
a-tph <aeronteoph@gmail.com>
2023-03-30 23:14:51 +0800

    Rewrite unit test to provide more clarity

--- test/test_account_verification.js
+++ test/test_account_verification.js
@@ -9,30 +9,27 @@ let accountOwner;
 let platformOwner;
 contract("AccountVerification", function (accounts) {
     before(async () => {
-        accountOwner = accounts[0];
-        platformOwner = accounts[1];
-        accountInstance = await Account.deployed({from:accountOwner});
-        platformInstance = await Platform.deployed({from:platformOwner});
+        accountInstance = await Account.deployed();
+        platformInstance = await Platform.deployed();
     });
     
     console.log("Testing Account Verification");
 
     it("Verifier is not certified", async () => {
         await truffleAssert.reverts(
-            platformInstance.verifyAccount(accounts[6], {from: accounts[5]}),
+            accountInstance.verifyAccount(accounts[6], {from: accounts[5]}),
             "Account not certified"
         );
     });
 
-    it("Ensure Platform can verify accounts (for listing event)", async () => {
-        // make platform contract a certfied party
-        let setPlatformAddr = await accountInstance.setPlatformAddress(platformOwner);
-        let certifyPlatform = await accountInstance.certifyAccount(platformOwner);
+    it("Account is certified to vote", async () => {
+        // make account[0] certified to verify organisations
+        let certify = await accountInstance.certifyAccount(accounts[0]);
 
-        // Set account 5 to be verified first using account contract called by platform contract
-        let verifyAccount5 = await accountInstance.verifyAccount(accounts[5], platformOwner, {from: platformOwner});
+        // Set account 5 to be verified using account[0]
+        let verifyAccount5 = await accountInstance.verifyAccount(accounts[5], {from: accounts[0]});
         assert(await accountInstance.viewAccountState(accounts[5]), await accountInstance.getVerifiedStatus() );
-        assert(await accountInstance.viewAccountVerifier(accounts[5]), platformOwner );
+        assert(await accountInstance.viewAccountVerifier(accounts[5]), accounts[0]);
     });
 
             
