3db162d14625f1f413c198c894833634e1250747
Tan Teck Hwee <tan.brian98@gmail.com>
2023-04-08 21:12:55 +0800

    Update comments to match test_platform

--- test/test_platform.js
+++ test/test_platform.js
@@ -287,7 +287,7 @@ contract("Platform", function (accounts) {
         let ownerEnd = await platformInstance.endSuccessfulEvent(latestEventId, {from: accounts[0]});
         truffleAssert.eventEmitted(ownerEnd, "OwnerEventEnd");
 
-        // Check that accounts recieved tokens
+        // Check that accounts recieved tokens earn from attending event
         const acc2token2 = new BigNumber(await eventTokenInstance.checkEventToken({from: accounts[2]}));
         await assert(acc2token2.isEqualTo(BigNumber(1)), "EventToken not minted");
         const acc3token2 = new BigNumber(await eventTokenInstance.checkEventToken({from: accounts[3]}));
@@ -308,7 +308,7 @@ contract("Platform", function (accounts) {
         let bidCommenced = await platformInstance.commenceBidding(latestEventId, {from: accounts[1]});
         truffleAssert.eventEmitted(bidCommenced, "BidCommenced");
         
-        // Generate token for accounts[2],[3] with 5,6 correspondingly
+        // Generate token for accounts[2],[3] with 4,5 correspondingly due to them earning tokens from event end previously
         await eventTokenInstance.getTokenForTesting(accounts[2], 4, {from: accounts[0]});
         await eventTokenInstance.getTokenForTesting(accounts[3], 5, {from: accounts[0]});
         const acc2token = new BigNumber(await eventTokenInstance.checkEventToken({from: accounts[2]}));
@@ -320,11 +320,11 @@ contract("Platform", function (accounts) {
         let bidPlaced1 = await platformInstance.placeBid(latestEventId, 1, 0, {from: accounts[3], value: oneEth});
         truffleAssert.eventEmitted(bidPlaced1, "BidPlaced");
 
-        // accounts[2] approve Platform contract to use 5 tokens and place bid for 1 ticket with 5 tokens each
+        // accounts[2] place bid for 1 ticket with 5 tokens each
         let bidPlaced2 = await platformInstance.placeBid(latestEventId, 1, 5, {from: accounts[2], value: oneEth});
         truffleAssert.eventEmitted(bidPlaced2, "BidPlaced");
 
-        // accounts[3] approve Platform contract to use 6 tokens and update bid for 1 ticket with 6 tokens each
+        // accounts[3] update bid for 1 ticket with 6 tokens each
         let updateBid1 = await platformInstance.updateBid(latestEventId, 6, {from: accounts[3]});
         truffleAssert.eventEmitted(updateBid1, "BidUpdate");
 
